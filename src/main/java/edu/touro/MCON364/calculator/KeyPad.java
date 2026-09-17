d40ca145d7da3803298585f775b4118a3e91453e
Naava <naavamp@gmail.com>
2026-09-16 20:11:54 -0400

    initial commit

---
+++ src/main/java/edu/touro/MCON364/calculator/KeyPad.java
@@ -0,0 +1,54 @@
+package edu.touro.MCON364.calculator;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class KeyPad
+{
+    static void main()
+    {
+        JFrame frame = new JFrame("Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //NORTH Input number
+
+        JPanel input = new JPanel(new GridLayout(0, 2, 4, 4));
+        input.add(new JTextField("", 10));
+
+        //Center Numbers and operations0;
+        JPanel numberButtons = new JPanel(new GridLayout(4, 3));
+        numberButtons.add(new JButton("0"));
+        numberButtons.add(new JButton("1"));
+        numberButtons.add(new JButton("2"));
+        numberButtons.add(new JButton("3"));
+        numberButtons.add(new JButton("4"));
+        numberButtons.add(new JButton("5"));
+        numberButtons.add(new JButton("6"));
+        numberButtons.add(new JButton("7"));
+        numberButtons.add(new JButton("8"));
+        numberButtons.add(new JButton("9"));
+
+        JPanel operationButtons = new JPanel(new GridLayout(0, 1));
+        operationButtons.add(new JButton("="));
+        operationButtons.add(new JButton("+"));
+        operationButtons.add(new JButton("-"));
+        operationButtons.add(new JButton("x"));
+        operationButtons.add(new JButton("/"));
+
+
+
+
+        JPanel content = new JPanel(new BorderLayout(0, 8));
+        content.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        content.add(input, BorderLayout.NORTH);
+        content.add(numberButtons, BorderLayout.CENTER);
+        content.add(operationButtons, BorderLayout.EAST);
+        frame.add(content);
+        frame.pack();
+
+        frame.setVisible(true);
+
+
+
+    }
+}
